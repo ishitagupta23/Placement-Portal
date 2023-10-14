55cc3d5d26eb9342cba065277cbd1f994fe72851
ishittaagupttaa <ishitaguptancr@gmail.com>
2023-10-14 14:20:41 +0530

    files added

---
+++ php/footer.php
@@ -0,0 +1,43 @@
+<footer id="footer" class="footer">
+
+
+
+    <div class="footer-legal text-center">
+        <div class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
+
+            <div class="d-flex flex-column align-items-center align-items-lg-start">
+                <div class="copyright">
+                    &copy; Copyright <strong><span>Placement Portal</span></strong>. All Rights Reserved
+                </div>
+                <div class="credits">
+
+                    <a href="https://bootstrapmade.com/">MSI-PlacementCell</a>
+                </div>
+            </div>
+
+            <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
+                <a href="https://twitter.com/MSI_JanakPuri58" class="twitter"><i class="bi bi-twitter"></i></a>
+                <a href="https://www.facebook.com/profile.php?id=100077721126796" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="https://www.instagram.com/thebcaplacementcell/" class="instagram"><i class="bi bi-instagram"></i></a>
+                <a href="https://www.linkedin.com/school/maharaja-surajmal-institute/" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            </div>
+
+        </div>
+    </div>
+
+</footer>
+
+<a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
